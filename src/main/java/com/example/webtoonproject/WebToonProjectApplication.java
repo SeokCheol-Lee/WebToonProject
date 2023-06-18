add3c38102cd260fa009f7c95d2ccf6fdb2d7be0
SeokCheol <lsc991123@gmail.com>
2023-06-18 18:16:14 +0900

    init

---
+++ src/main/java/com/example/webtoonproject/WebToonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtoonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebToonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webtoonproject/WebToonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtoonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
